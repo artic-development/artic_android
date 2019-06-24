a646c25da92d6d81169581346b18c028dea0b2e3
greedy0110 <greedy0110@gmail.com>
2019-06-24 19:38:14 +0900

    first commit of artic

---
+++ app/src/main/java/com/android/artic/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.artic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
