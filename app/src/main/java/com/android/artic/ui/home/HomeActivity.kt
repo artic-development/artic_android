143bae84f5d3850c121b7ab21ba1cd1885e21044
greedy0110 <greedy0110@gmail.com>
2019-06-30 21:53:32 +0900

    delete home card adapter & layout & repository call
    my heart is also gone :(

--- app/src/main/java/com/android/artic/ui/home/HomeActivity.kt
+++ app/src/main/java/com/android/artic/ui/home/HomeActivity.kt
@@ -1,51 +1,18 @@
 package com.android.artic.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.artic.R
-import com.android.artic.repository.ArticRepository
 import com.android.artic.ui.BaseActivity
-import com.android.artic.ui.home.data.HomeCard
-import kotlinx.android.synthetic.main.activity_home.*
-import org.jetbrains.anko.toast
-import org.koin.android.ext.android.inject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
+/**
+ * 1. Home Fragment 들을 관리할 것
+ * 2. Fragment 들의 위치는 유동적으로 변경가능 하며 HomeActivity 가 관리할 것
+ * 3. Search Activity 를 시작할 수 있어야
+ * @author greedy0110
+ * */
 class HomeActivity : BaseActivity() {
-    private val repository: ArticRepository by inject()
-    private val adapter: HomeCardAdapter by lazy { HomeCardAdapter(this, listOf()) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        // adapter, recyclerView setting
-        rv_home_card.adapter = adapter
-        rv_home_card.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        // Observable version
-        //repository.getHomeCardList()
-        //    .subscribe {
-        //        adapter.data = it
-        //        adapter.notifyDataSetChanged()
-        //    }.apply { addDisposable(this) }
-
-        repository.getHomeCardList().enqueue(object : Callback<List<HomeCard>> {
-            override fun onFailure(call: Call<List<HomeCard>>, t: Throwable) {
-                toast("홈 카드 업데이트 실패")
-            }
-
-            override fun onResponse(call: Call<List<HomeCard>>, response: Response<List<HomeCard>>) {
-                response.body()?.let {
-                    adapter.data = it
-                    adapter.notifyDataSetChanged()
-                }
-            }
-        })
     }
 }
-
-
--- app/src/main/java/com/android/artic/ui/home/data/HomeCardKind.kt
+++
@@ -1,5 +0,0 @@
-package com.android.artic.ui.home.data
-
-enum class HomeCardKind {
-    ARCHIVE, LINK
-}
