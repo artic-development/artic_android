1d44272b75cb968caceeee50922abf84e47b8996
greedy0110 <greedy0110@gmail.com>
2019-07-01 01:06:46 +0900

    implement category_archive fragment & category archive card list view
    replace home_activity top layer to ScrollView
    implement dynamic fragment placing in HomeActivity
    write guideline HOW TO ADD FRAGMENT TO ACTIVITY writing some comment :) i want to it will help you guys

--- app/src/main/java/com/android/artic/ui/home/HomeActivity.kt
+++ app/src/main/java/com/android/artic/ui/home/HomeActivity.kt
@@ -3,6 +3,8 @@ package com.android.artic.ui.home
 import android.os.Bundle
 import com.android.artic.R
 import com.android.artic.ui.BaseActivity
+import com.android.artic.ui.home.category_archive.CategoryArchiveFragment
+import com.android.artic.ui.home.new_archive.NewArchiveFragment
 
 /**
  * 1. Home Fragment 들을 관리할 것
@@ -14,5 +16,17 @@ class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // 일단 샘플로 홈화면 구성해봤어. fragmentManager add 를 사용해서 각자 만든 fragment 들을 계속 vertical linearLayout에 그려주는 형태로 구성함
+        // HomeActivity 도 전체 Scroll 로 감싸서 구성해놨으니 이거 기반으로 작업 진행해도 괜찮을듯
+        // 프래그먼트로 데이터를 던져줘야 하는 경우가 생기면 이것저것 찾아보는데, 난 그냥 생성자로 넘겨버리는 형태로 구성했음! - 승민
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.add(R.id.container_home_fragments, NewArchiveFragment(), "new_archive_fragment")
+
+        val categoryName = "Design"
+        transaction.add(R.id.container_home_fragments ,CategoryArchiveFragment(0, categoryName), "category_archive_$categoryName")
+
+        transaction.commit()
     }
 }
