c31f586accc817057549cb566e6cd25764c60a1b
ChoSooMin <vywlgns1102@naver.com>
2019-07-02 02:37:11 +0900

    home doing

--- app/src/main/java/com/android/artic/ui/home/HomeActivity.kt
+++ app/src/main/java/com/android/artic/ui/home/HomeActivity.kt
@@ -1,12 +1,16 @@
 package com.android.artic.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import com.android.artic.R
 import com.android.artic.ui.BaseActivity
+import com.android.artic.ui.home.artic_pick.ArticPickFragment
 import com.android.artic.ui.home.category_archive.CategoryArchiveFragment
 import com.android.artic.ui.home.new_archive.NewArchiveFragment
 import com.android.artic.ui.home.new_article.NewArticleFragment
 import com.android.artic.ui.home.reading_history.ReadingHistoryFragment
+import com.android.artic.ui.search.SearchActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 /**
  * 1. Home Fragment 들을 관리할 것
@@ -24,16 +28,27 @@ class HomeActivity : BaseActivity() {
         // 프래그먼트로 데이터를 던져줘야 하는 경우가 생기면 이것저것 찾아보는데, 난 그냥 생성자로 넘겨버리는 형태로 구성했음! - 승민
         val transaction = supportFragmentManager.beginTransaction()
 
-        transaction.add(R.id.container_home_fragments, NewArchiveFragment(), "new_archive_fragment")
+        // 순서) 새로운 아카이브, 새로운 아티클, 최근 읽은 아티클, 아틱의 선택, 카테고리별
+
+        transaction.add(R.id.container_home_fragments, NewArchiveFragment(), "new_archive_fragment") // 새로운 아카이브
         // 수민 추가
-        transaction.add(R.id.container_home_fragments, NewArticleFragment(), "new_article_fragment")
+        transaction.add(R.id.container_home_fragments, NewArticleFragment(), "new_article_fragment") // 새로운 아티클
 
 
-        transaction.add(R.id.container_home_fragments,ReadingHistoryFragment(),"reading_history_fragment")
+        transaction.add(R.id.container_home_fragments, ReadingHistoryFragment(),"reading_history_fragment") // 최근 읽은 아티클
+
+        transaction.add(R.id.container_home_fragments, ArticPickFragment(), "artic_pick_fragment") // 아틱의 선택
+
+        // 카테고리별
         val categoryName = "Design"
         transaction.add(R.id.container_home_fragments ,CategoryArchiveFragment(0, categoryName), "category_archive_$categoryName")
 
 
         transaction.commit()
+
+        constraintLayout.setOnClickListener {
+            var intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
