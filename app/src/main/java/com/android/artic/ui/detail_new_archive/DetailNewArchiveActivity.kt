443ca71e78ff020e8c3b07240b2f37fdaefda0fe
ChoSooMin <vywlgns1102@naver.com>
2019-07-05 05:10:47 +0900

    link Login, Signup, HomeActivity complete

--- app/src/main/java/com/android/artic/ui/detail_new_archive/DetailNewArchiveActivity.kt
+++ app/src/main/java/com/android/artic/ui/detail_new_archive/DetailNewArchiveActivity.kt
@@ -2,12 +2,13 @@ package com.android.artic.ui.detail_new_archive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.artic.R
 import com.android.artic.data.Archive
 import com.android.artic.repository.ArticRepository
 import com.android.artic.ui.BaseActivity
 import com.android.artic.ui.adapter.archive.ArchiveListAdapter
-import com.android.artic.ui.search_result.ArchiveListFragment
+import kotlinx.android.synthetic.main.activity_detail_new_archive.*
 import org.jetbrains.anko.toast
 import org.koin.android.ext.android.inject
 import retrofit2.Call
@@ -21,6 +22,9 @@ class DetailNewArchiveActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_new_archive)
+
+        rv_detail_new_archive_list.adapter = adapter
+        rv_detail_new_archive_list.layoutManager = LinearLayoutManager(this)
 
         repository.getNewArchiveList().enqueue(
             object : Callback<List<Archive>> {
--- app/src/main/java/com/android/artic/ui/home/category_archive/CategoryArchiveFragment.kt
+++ app/src/main/java/com/android/artic/ui/home/category_archive/CategoryArchiveFragment.kt
@@ -1,6 +1,7 @@
 package com.android.artic.ui.home.category_archive
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.android.artic.R
 import com.android.artic.data.Archive
 import com.android.artic.repository.ArticRepository
+import com.android.artic.ui.archive.ArchiveActivity
 import kotlinx.android.synthetic.main.fragment_category_archive.*
 import org.jetbrains.anko.toast
 import org.koin.android.ext.android.inject
@@ -49,8 +51,16 @@ class CategoryArchiveFragment(
 
             txt_category_archive_name.text = categoryName
             rv_category_archive.adapter = adapter
+
             // 2x2 를 만들어줘야 하므로 데이터는 앞의 4개만 받아오자.
             rv_category_archive.layoutManager = GridLayoutManager(this, 2)
+
+            // @수민) 카테고리 누르면 해당 카테고리에 해당하는 아카이브 리스트가 있는 페이지로
+            linear_fragment_category_archive_category.setOnClickListener {
+                var intent = Intent(this, ArchiveActivity::class.java)
+
+                startActivity(intent)
+            }
 
             repository.getArchiveListGivenCategory(categoryId).enqueue(
                 object : Callback<List<Archive>> {
