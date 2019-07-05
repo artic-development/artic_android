930f833beb1e3133cdbdb91e7cea71884f56ece4
songkyunghee <kyunghee794613@gmail.com>
2019-07-06 02:57:10 +0900

    Merge branch 'develop' of https://github.com/artic-development/artic_android into develop
    edit profile  modify
    mypage modify in processing
    # Conflicts:
    #	app/src/main/res/drawable-xxxhdpi/tab_category.png
    #	app/src/main/res/drawable-xxxhdpi/tab_category_filled.png
    #	app/src/main/res/drawable-xxxhdpi/tab_home.png
    #	app/src/main/res/drawable-xxxhdpi/tab_notice.png
    #	app/src/main/res/drawable-xxxhdpi/tab_notice_filled.png
    #	app/src/main/res/drawable-xxxhdpi/tab_profile.png

--- app/src/main/java/com/android/artic/ui/setting/setting/SettingActivity.kt
+++ app/src/main/java/com/android/artic/ui/setting/setting/SettingActivity.kt
@@ -1,13 +1,24 @@
 package com.android.artic.ui.setting.setting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.artic.R
+import com.android.artic.ui.setting.setting_editprofile.SettingEditProfileActivity
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+
+        //프로필 수정 버튼 누르면 프로필 수정 페이지로
+        setting_edit_profile_container.setOnClickListener{
+            val intent= Intent(this, SettingEditProfileActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/android/artic/ui/setting/setting_editprofile/SettingEditProfileActivity.kt
+++ app/src/main/java/com/android/artic/ui/setting/setting_editprofile/SettingEditProfileActivity.kt
@@ -1,13 +1,124 @@
 package com.android.artic.ui.setting.setting_editprofile
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.android.artic.R
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
 
 class SettingEditProfileActivity : AppCompatActivity() {
 
+    private var btn: TextView?= null
+    private var imageview: ImageView? = null
+    private val GALLERY=1
+    private val CAMERA =2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_edit_profile)
+
+
+        btn = findViewById<View>(R.id.edit_profile_img_change_btn) as TextView
+        imageview= findViewById<View>(R.id.edit_profile_img) as ImageView
+
+        btn!!.setOnClickListener{
+            choosePhotoFromGallary()
+
+        }
+
     }
+
+    //갤러리에서 이미지를 선택하면 onActivityResult() 메소드가 실행
+    fun choosePhotoFromGallary() {
+        val galleryIntent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(galleryIntent, GALLERY)
+    }
+
+
+    public override fun onActivityResult(requestCode:Int, resultCode:Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY)
+        {
+            if (data != null)
+            {
+                val contentURI = data!!.data
+                try
+                {
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                    val path = saveImage(bitmap)
+                    Toast.makeText(this, "Image Saved!", Toast.LENGTH_SHORT).show()
+                    imageview!!.setImageBitmap(bitmap)
+
+                }
+                catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+
+    }
+
+    //이미지를 저장하는 메소드
+    fun saveImage(myBitmap: Bitmap):String {
+        val bytes = ByteArrayOutputStream()
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes)
+        val wallpaperDirectory = File(
+            (Environment.getExternalStorageDirectory()).toString() + IMAGE_DIRECTORY)
+        // have the object build the directory structure, if needed.
+        Log.d("fee",wallpaperDirectory.toString())
+        if (!wallpaperDirectory.exists())
+        {
+
+            wallpaperDirectory.mkdirs()
+        }
+
+        try
+        {
+            Log.d("heel",wallpaperDirectory.toString())
+            val f = File(wallpaperDirectory, ((Calendar.getInstance()
+                .getTimeInMillis()).toString() + ".jpg"))
+            f.createNewFile()
+            val fo = FileOutputStream(f)
+            fo.write(bytes.toByteArray())
+            MediaScannerConnection.scanFile(this,
+                arrayOf(f.getPath()),
+                arrayOf("image/jpeg"), null)
+            fo.close()
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath())
+
+            return f.getAbsolutePath()
+        }
+        catch (e1: IOException) {
+            e1.printStackTrace()
+        }
+
+        return ""
+    }
+
+    companion object {
+        private val IMAGE_DIRECTORY = "/demonuts"
+    }
+
 }
+
