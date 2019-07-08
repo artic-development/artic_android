b474220cf1be70a1b3169768a49a3fd945ebc685
greedy0110 <greedy0110@gmail.com>
2019-07-09 04:59:02 +0900

    implement logout

--- app/src/main/java/com/android/artic/ui/setting/setting/SettingActivity.kt
+++ app/src/main/java/com/android/artic/ui/setting/setting/SettingActivity.kt
@@ -4,16 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.artic.R
+import com.android.artic.auth.Auth
+import com.android.artic.ui.login.login_start.LoginStartActivity
 import com.android.artic.ui.setting.setting_editprofile.SettingEditProfileActivity
 import com.android.artic.ui.setting.setting_version.SettingVersionActivity
 import kotlinx.android.synthetic.main.activity_setting.*
+import org.jetbrains.anko.clearTask
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+import org.jetbrains.anko.startActivity
+import org.koin.android.ext.android.inject
 
 class SettingActivity : AppCompatActivity() {
+    private val auth: Auth by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
+        setting_artic_account_logout.setOnClickListener {
+            auth.logout {
+                // TODO 첫 화면으로 이동하고, 기존의 Activity Stack을 전부 정리해준다. 스플래시 화면이 나오면 스플래쉬로 이동해야하나?
+                startActivity(intentFor<LoginStartActivity>().newTask().clearTask())
+            }
+        }
 
         //프로필 수정 버튼 누르면 프로필 수정 페이지로
         setting_edit_profile_container.setOnClickListener{
