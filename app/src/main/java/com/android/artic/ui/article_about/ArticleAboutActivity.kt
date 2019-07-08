42002d973c8846d125215957718c8fe2789f7037
greedy0110 <greedy0110@gmail.com>
2019-07-08 18:59:23 +0900

    implement [server connection] get article by id (ArticleWebViewActivity)
    /home/article/{article_idx}/new
    gardening ArticleWebViewActivity

--- app/src/main/java/com/android/artic/ui/article_about/ArticleAboutActivity.kt
+++ app/src/main/java/com/android/artic/ui/article_about/ArticleAboutActivity.kt
@@ -8,6 +8,7 @@ import com.android.artic.data.Article
 import com.android.artic.logger.Logger
 import com.android.artic.repository.ArticRepository
 import com.android.artic.ui.BaseActivity
+import com.android.artic.ui.article.ArticleActivity
 import com.android.artic.ui.article_webview.ArticleWebViewActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_article_about.*
@@ -19,7 +20,6 @@ import retrofit2.Callback
 import retrofit2.Response
 
 /**
- * it must need article id (intent["articleId"])
  * it must need archive id (intent["archiveId"])
  * @author greedy0110
  * */
@@ -32,65 +32,61 @@ class ArticleAboutActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_about)
 
-        val archiveId = intent.getIntExtra("archiveId", -1)
         val articleId = intent.getIntExtra("articleId", -1)
 
-        logger.log("archiveId $archiveId , articleId $articleId")
+        logger.log("ArticleAboutActivity articleId $articleId")
 
-        if (articleId == -1 || archiveId == -1) {
+        if (articleId == -1) {
             //toast(R.string.network_error)
             //finish()
         }
-
-        repository.getArchiveName(archiveId).enqueue(
-            object : Callback<String> {
-                override fun onFailure(call: Call<String>, t: Throwable) {
-                    toast(R.string.network_error)
-                }
-
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    response.body()?.let{
-                        txt_article_about_archive_title.text = it
-                    }
-                }
-            }
-        )
-
-        repository.getArticle(articleId = articleId,
-            successCallback = {
-                txt_article_about_url.text = it.url
-                // TODO 타이틀 길이가 너무 길면 안된다! 알아서 줄여주는 방안을 생각해야함.
-                txt_article_about_title.text = title
-                Glide.with(this@ArticleAboutActivity)
-                    .load(it.title_img_url)
-                    .into(img_article_about)
-
-                btn_article_about_read.setOnClickListener {
-                    startActivity<ArticleWebViewActivity>("articleId" to it.id)
-                }
-            },
-            failCallback = {
-                toast(R.string.network_error)
-            })
 
         rv_article_about_another_article.adapter = adapter
         // 2x2 를 만들어줘야 하므로 데이터는 앞의 4개만 받아오자.
         rv_article_about_another_article.layoutManager = GridLayoutManager(this, 2)
 
-        repository.getArticleListGivenArchive(
-            archiveId = archiveId,
-            successCallback = {
-                adapter.data = it
-                adapter.notifyDataSetChanged()
+        repository.getArticleWithExtra(
+            articleId = articleId,
+            successCallback = { article, archivePair ->
+                txt_article_about_url.text = article.url
+                // TODO 타이틀 길이가 너무 길면 안된다! 알아서 줄여주는 방안을 생각해야함.
+                txt_article_about_title.text = title
+                Glide.with(this@ArticleAboutActivity)
+                    .load(article.title_img_url)
+                    .into(img_article_about)
+
+                btn_article_about_read.setOnClickListener {
+                    startActivity<ArticleWebViewActivity>("articleId" to articleId)
+                }
+
+                txt_article_about_archive_title.text = archivePair.second
+
+                // 비동기 통신의 연속
+                archivePair.first?.let { archiveId ->
+                    logger.log("ArticleAboutActivity $archiveId")
+                    repository.getArticleListGivenArchive(
+                        archiveId = archiveId,
+                        successCallback = {
+                            adapter.data = it
+                            adapter.notifyDataSetChanged()
+
+                            btn_article_about_show_all.setOnClickListener {
+                                startActivity<ArticleActivity>(
+                                    "archiveId" to archiveId,
+                                    "categoryTitle" to "", // TODO category Title 도 받아와야하나?
+                                    "archiveTitle" to archivePair.second
+                                )
+                            }
+                        },
+                        failCallback = {
+                            toast(R.string.network_error)
+                        }
+                    )
+                }
             },
             failCallback = {
                 toast(R.string.network_error)
             }
         )
-
-        btn_article_about_show_all.setOnClickListener {
-            // TODO archiveId 를 사용해서 아카이브 전체 보기 화면으로 넘어가야함.
-        }
-
     }
 }
